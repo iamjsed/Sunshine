165c95a4654acb9a3fc5e670091020042479f7d1
iamjsed <julius.sediolano@gmail.com>
2015-09-07 16:24:58 -0700

    Started with Lesson 3, added an Intent to launch the Forecast Details Activity (with simple Text View).
    Added, Settings Activity.

--- app/src/main/java/iam/jsed/sunshine/MainActivity.java
+++ app/src/main/java/iam/jsed/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package iam.jsed.sunshine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
